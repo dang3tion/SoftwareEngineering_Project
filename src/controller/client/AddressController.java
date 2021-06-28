2286879e4e02941d9e231df7209bb27e787ee4c5
robocon321 <robocon321n@gmail.com>
2021-06-28 22:40:24 +0700

    Update Search&Filter

--- src/controller/client/AddressController.java
+++ src/controller/client/AddressController.java
@@ -18,8 +18,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
 
 @WebServlet(urlPatterns = { "/address" })
 public class AddressController extends HttpServlet {
